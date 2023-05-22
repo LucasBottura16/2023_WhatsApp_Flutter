cc408c358bb303ea461e252958cbf017a3a92ef4
LucasBottura16 <bdfbottura@gmail.com>
2023-05-21 23:25:53 -0300

    feat: Criação de tela de configuração para salvar e atualizar fotos e dados

--- lib/configuracoes.dart
+++ lib/configuracoes.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
 
 class Configuracoes extends StatefulWidget {
   const Configuracoes({Key? key}) : super(key: key);
@@ -8,8 +13,104 @@ class Configuracoes extends StatefulWidget {
 }
 
 class _ConfiguracoesState extends State<Configuracoes> {
-
   final TextEditingController _controllerNome = TextEditingController();
+  late File _imagem;
+  ImagePicker picker = ImagePicker();
+  String? _uidUsuario;
+  String? _urlRecuperada;
+  bool _subindoImagem = false;
+
+  Future? _recuperarImagem(String origemImagem) async {
+    File? imagemSelecionada;
+    switch (origemImagem) {
+      case "Camera":
+        final pickedFile = await picker.pickImage(source: ImageSource.camera);
+        imagemSelecionada = File(pickedFile!.path);
+        break;
+      case "Galeria":
+        final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+        imagemSelecionada = File(pickedFile!.path);
+        break;
+    }
+
+    setState(() {
+      _imagem = imagemSelecionada!;
+    });
+
+    _uploadImage();
+  }
+
+  Future? _uploadImage() async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference pastaRaiz = storage.ref();
+    Reference arquivo = pastaRaiz.child("Perfil").child("$_uidUsuario.jpg");
+
+    UploadTask task = arquivo.putFile(_imagem);
+
+    task.snapshotEvents.listen((TaskSnapshot storageEvent) {
+      if (storageEvent.state == TaskState.running) {
+        setState(() {
+          _subindoImagem = true;
+        });
+      } else if (storageEvent.state == TaskState.success) {
+        _subindoImagem = false;
+      }
+    });
+
+    //Recuperar URL da imagem
+    task.then((TaskSnapshot taskSnapshot) {
+      _recuperarUrlImagem(taskSnapshot);
+    });
+  }
+
+  Future? _recuperarUrlImagem(TaskSnapshot taskSnapshot) async {
+    String url = await taskSnapshot.ref.getDownloadURL();
+    _salvarImagem(url);
+
+    setState(() {
+      _urlRecuperada = url;
+    });
+  }
+
+  _salvarNome() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    String nome = _controllerNome.text;
+
+    Map<String, dynamic> dadosAtualizados = {"nome": nome};
+
+    firestore.collection("Clientes").doc(_uidUsuario).update(dadosAtualizados);
+  }
+
+  _salvarImagem(String url) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    Map<String, dynamic> dadosAtualizados = {"URL": url};
+
+    firestore.collection("Clientes").doc(_uidUsuario).update(dadosAtualizados);
+  }
+
+  _recuperaDados() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? userLogado = auth.currentUser;
+    _uidUsuario = userLogado?.uid;
+
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    await firestore.collection("Clientes").doc(_uidUsuario).get().then((value) {
+      setState(() {
+        _controllerNome.text = value.get("nome");
+        if (value.get("URL") != null) {
+          _urlRecuperada = value.get("URL");
+        }
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _recuperaDados();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,29 +120,37 @@ class _ConfiguracoesState extends State<Configuracoes> {
         backgroundColor: const Color(0xff075E54),
       ),
       body: Container(
-        padding:const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const CircleAvatar(
-                  radius: 100,
-                  backgroundColor: Colors.grey,
-                  backgroundImage:
-                  NetworkImage("https://firebasestorage.googleapis.com/v0/b/fir-flutter-8e6f4.appspot.com/o/Perfil%2Fperfil4.jpg?alt=media&token=8a7f2564-d5ef-4aff-b3ee-7d893144a90e")
-                ),
+                _subindoImagem
+                    ? const CircularProgressIndicator()
+                    : Container(),
+                const SizedBox(height: 10),
+                CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: _urlRecuperada != null
+                        ? NetworkImage(_urlRecuperada.toString())
+                        : null),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     TextButton(
-                        onPressed: (){},
-                        child: const Text("Camera")
+                        onPressed: () {
+                          _recuperarImagem("Camera");
+                        },
+                        child: const Text("Camera")),
+                    const SizedBox(
+                      width: 20,
                     ),
-                    const SizedBox(width: 20,),
                     TextButton(
-                        onPressed: (){},
-                        child: const Text("Galeria")
-                    ),
+                        onPressed: () {
+                          _recuperarImagem("Galeria");
+                        },
+                        child: const Text("Galeria")),
                   ],
                 ),
                 const SizedBox(height: 10),
@@ -62,6 +171,27 @@ class _ConfiguracoesState extends State<Configuracoes> {
                     ),
                   ),
                   controller: _controllerNome,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.fromLTRB(32, 16, 32, 16)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32), // Define o raio dos cantos do botão
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    _salvarNome();
+                  },
+                  child: const Text(
+                    "Salvar",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
                 ),
               ],
             ),
